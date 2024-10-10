3ba112c918d268030d85e91b25fa1e8f446e4fd7
Yehiya <yehiya@zumisolutions.com>
2024-10-10 19:22:48 +0530

    Instrumentation Tests Error is Resolved 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
