24ab833beedb3e6b12cbcfac2ea5cb8095a18a5c
Yehiya <yehiya@zumisolutions.com>
2024-10-11 17:10:04 +0530

    Added SonarQube

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,13 +10,32 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        // Add jcenter if you still need it (note that it is deprecated)
+        jcenter()
+        maven {
+            url = uri("https://maven.google.com/")
+            name = "Google"
+        }
+    }
+
+    // Adding classpath dependencies for the plugins
+    plugins {
+        id("com.android.application") version "4.0.2"
+        id("org.sonarqube") version "2.7.1"
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        // Add jcenter if needed
+        jcenter()
+        maven {
+            url = uri("https://maven.google.com/")
+            name = "Google"
+        }
     }
 }
 
